b4846be8d3c510f2173831b9ad500d6fd667304d
blerud <lliu301@gmail.com>
2015-03-30 18:05:15 -0700

    initial commit

---
+++ soundboard.py
@@ -0,0 +1,28 @@
+import os
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    data = get_sounds()
+    return render_template('layout.html', data=data)
+
+def get_sounds():
+    sounds = {}
+    db = open('static/smashaudio/ghettodb.txt', 'r')
+    for line in db:
+        line = line.strip()
+        if line == '':
+            break
+        numfiles = -1
+        if line.split(' ')[0] == 'd':
+            numfiles = len([n for n in os.listdir('static/smashaudio/'+line.split(' ')[1]) if n.endswith('.wav')])
+        sounds[line.split(' ')[1]] = (line.split(' ')[0], ' '.join(line.split(' ')[2:]), numfiles)
+    return sounds
+
+if __name__ == '__main__':
+    get_sounds()
+    app.run()
+
